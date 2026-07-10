8c859b5db5acbbf90a72ab4cb6203cb0276c8422
leocodes-al <leonardo.sistemainformacao@gmail.com>
2026-07-10 15:47:35 -0300

    Desenvolvendo a estrutura do Sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,27 +1,71 @@
+import { NavLink } from "react-router-dom"
+import { LayoutDashboard, Dumbbell, Footprints, LineChart, Wallet, User } from "../../assets/icons/lucideIcons"
+import Logo from "../../assets/images/logo.svg"
+
 import "./Sidebar.css";
 
 function Sidebar() {
 
-  const lista_sidebar = ["Dashboard", "Meu Treino", "Corridas", "Progresso Físico", "Finacias", "Perfil"]
+  const sidebarItems = [
+    {
+      nome: "Dashboard",
+      rota: "/dashboard",
+      icon: LayoutDashboard,
+    },
+    {
+      nome: "Meu Treino",
+      rota: "/meu-treino",
+      icon: Dumbbell,
+    },
+    {
+      nome: "Corridas",
+      rota: "/corridas",
+      icon: Footprints,
+    },
+    {
+      nome: "Evolução",
+      rota: "/evolucao",
+      icon: LineChart,
+    },
+    {
+      nome: "Financeiro",
+      rota: "/financeiro",
+      icon: Wallet,
+    },
+    {
+      nome: "Perfil",
+      rota: "/perfil",
+      icon: User,
+    },
+  ]
 
   return (
     <aside className="sidebar">
 
       <div className="sidebar__logo">
-        FitTrack Pro
+        <img src={Logo} alt="Logo FitTrack Pro" />
+        <h4>FitTrack <span>Pro</span></h4>
       </div>
 
       <nav className="sidebar__nav">
-        <ul>
+        <ul className="sidebar__list">
 
-          {lista_sidebar.map((item, index) => (
-            <li key={index}>
-              <a href="#">{item}</a>
+          {sidebarItems.map((item) => (
+            <li key={item.rota} className="sidebar__item">
+              <NavLink to={item.rota} className="sidebar__link">
+                <item.icon className="sidebar__icon" />
+                {item.nome}
+              </NavLink>
             </li>
           ))}
 
         </ul>
       </nav>
+
+      <div className="sidebar__avatar">
+        <h4>Leonardo</h4>
+        <p>Student</p>
+      </div>
 
     </aside>
   );
